0b4cf08e0cee03229a82c2c5e6a38126cc5412e3
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2023-03-22 12:08:19 -0700

    added header and footer

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+	require('db.php');
+	include 'header.php';
+	echo "<center><h3>Welcome to my Awesome Website!</h3></center>";
+	echo '<center><div>New user? Please <a href="login.php">Login</a> or <a href="register.php">Register</a> </div></center>';
+	include 'footer.php';
+?>
